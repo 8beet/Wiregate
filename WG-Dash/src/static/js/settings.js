794ffe333b1c24d5ad250048584ad0f4a9c1f83c
Noxcis <shamarakeillee@gmail.com>
2023-12-10 02:00:03 -0800

    cl

--- WG-Dash/src/static/js/settings.js
+++ WG-Dash/src/static/js/settings.js
@@ -1,22 +1,24 @@
-$(".sb-settings-url").addClass("active")
+$(".sb-settings-url").addClass("active");
+
 $(".confirm_modal").click(function () {
-    $(".app_new_ip").html($("#app_ip")[0].value)
-    $(".app_new_port").html($("#app_port")[0].value)
-})
+    $(".app_new_ip").text($("#app_ip")[0].value);
+    $(".app_new_port").text($("#app_port")[0].value);
+});
 
 $(".confirm_restart").click(function () {
-    $(".cancel_restart").remove()
+    $(".cancel_restart").remove();
     countdown = 7;
-    $.post('/update_app_ip_port', $('.update_app_ip_port').serialize())
+    $.post('/update_app_ip_port', $('.update_app_ip_port').serialize());
     url = $("#app_ip")[0].value + ":" + $("#app_port")[0].value;
-    $(".confirm_restart").attr("disabled", "disabled")
+    $(".confirm_restart").attr("disabled", "disabled");
+
     setInterval(function () {
         if (countdown === 0) {
             window.location.replace("http://" + url);
         }
-        $(".confirm_restart").html("Redirecting you in " + countdown + " seconds.")
+        $(".confirm_restart").text("Redirecting you in " + countdown + " seconds.");
         countdown--;
-    }, 1000)
+    }, 1000);
 });
 
 $(".change_path").click(function () {
@@ -24,16 +26,15 @@ $(".change_path").click(function () {
     countdown = 5;
     setInterval(function () {
         if (countdown === 0) {
-            location.reload()
+            location.reload();
         }
-        $(".change_path").html("Redirecting you in " + countdown + " seconds.")
+        $(".change_path").text("Redirecting you in " + countdown + " seconds.");
         countdown--;
-    }, 1000)
-    $.post('/update_wg_conf_path', $('.update_wg_conf_path').serialize())
+    }, 1000);
+    $.post('/update_wg_conf_path', $('.update_wg_conf_path').serialize());
 });
 
 $(".bottomNavSettings").addClass("active");
-
 
 $(".theme-switch-btn").on("click", function(){
     if (!$(this).hasClass("active")){
@@ -49,7 +50,7 @@ $(".theme-switch-btn").on("click", function(){
             if (res.status == true){
                 if (theme == "light"){
                     $("#darkThemeCSS").remove();
-                }else{
+                } else {
                     $("head").append('<link rel="stylesheet" type="text/css" href="/static/css/theme/dark.min.css" id="darkThemeCSS">');
                 }
             }
